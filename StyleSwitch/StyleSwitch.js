43658e2cd3ccbe35a3e13e21b293e1a7fa692553
Steven Schifter <70248147+StevenSchifter@users.noreply.github.com>
2023-09-25 13:48:10 -0400

    Overhauled StyleSwitch
    Now uses localStorage to toggle the dark scheme. Inspired by: https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/

--- StyleSwitch/StyleSwitch.js
+++ StyleSwitch/StyleSwitch.js
@@ -1,36 +1,22 @@
-var inDarkMode = 0;
-
-function toggleMode()
-{
-    inDarkMode = 1 - inDarkMode;
-    setMode(inDarkMode);
-}
-
-function setMode(myDarkMode)
-{
-    if(myDarkMode == 1)
-    {
-        document.querySelector("body").classList.add("darkmode");
-        document.querySelector("a").classList.add("darklink");
-        document.querySelector("#currentMode").innerText = "🌙 Dark Mode";
-    }
-    else if(myDarkMode == 0)
-    {
-        document.querySelector("body").classList.remove("darkmode");
-        document.querySelector("a").classList.remove("darklink");
-        document.querySelector("#currentMode").innerText = "☀ Light Mode";
-    }
-}
-
-// Some code borrowed from https://flaviocopes.com/javascript-detect-dark-mode/
-
 function detectDarkMode()
 {
-    if(window.matchMedia)
+    const usingDarkScheme = localStorage.getItem("usingDarkScheme");
+    if (usingDarkScheme == 1)
     {
-        if(window.matchMedia('(prefers-color-scheme: dark)').matches)
-        {
-            toggleMode();
-        }
+        document.body.classList.add("dark-scheme");
     }
+}
+
+function toggleScheme()
+{
+    document.body.classList.toggle("dark-scheme");
+    
+    let selectedDarkScheme = 0;
+    
+    if (document.body.classList.contains("dark-scheme"))
+    {
+        let selectedDarkScheme = 1;
+    }
+    
+    localStorage.setItem("usingDarkScheme", selectedDarkScheme);
 }
