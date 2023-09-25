e7c3405af58e71384503e11040d0b1b5f86f6087
Steven Schifter <70248147+StevenSchifter@users.noreply.github.com>
2023-09-25 14:39:27 -0400

    Update StyleSwitch.js
    Added comments to StyleSwitch.js
    Re-added OS dark mode detection
    Re-wrote toggleScheme()

--- StyleSwitch/StyleSwitch.js
+++ StyleSwitch/StyleSwitch.js
@@ -1,21 +1,34 @@
+// Detect dark scheme preference or OS dark mode setting
 function detectDarkMode()
 {
+    // First check local storage for dark scheme preference
     if (localStorage.getItem("usingDarkScheme") === "true")
     {
         document.body.classList.add("dark-scheme");
     }
+    
+    // If no preference was found, check OS dark mode setting and use that to set scheme and preference
+    // Media-matching code borrowed from https://flaviocopes.com/javascript-detect-dark-mode/
+    else if (localStorage.getItem("usingDarkScheme") === null && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    {
+        document.body.classList.add("dark-scheme");
+        localStorage.setItem("usingDarkScheme", "true");
+    }
 }
 
+// Switch between light and dark schemes 
 function toggleScheme()
 {
+    // Add or remove the dark scheme CSS class
     document.body.classList.toggle("dark-scheme");
     
-    let selectedDarkScheme = "false";
-    
+    // Set dark scheme preference in local storage
     if (document.body.classList.contains("dark-scheme"))
     {
-        selectedDarkScheme = "true";
+        localStorage.setItem("usingDarkScheme", "true");
     }
-    
-    localStorage.setItem("usingDarkScheme", selectedDarkScheme);
+    else
+    {
+        localStorage.setItem("usingDarkScheme", "false");
+    }
 }
