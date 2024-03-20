965276ceaa988c4ccb9adc4533ee0fb8e5d4ff47
Steven Schifter <70248147+StevenSchifter@users.noreply.github.com>
2024-03-20 15:36:29 -0400

    Significant update to JSON example
    Originally, the returned array was simply each field's contents quoted.
    In this update, the array consists of key-value pairs, with the field label as the key and the field's contents as the value.

--- ArrayToJSON/ArrayToJSON.js
+++ ArrayToJSON/ArrayToJSON.js
@@ -1,33 +1,34 @@
 "use strict";
 
 // This function checks whether any of the input fields are blank.
-function validateElements()
+function validateFields()
 {
-    // This array of attributes is generated from the input field values when the button is clicked.
-    const myAttributes = [
-        document.getElementById("a1").value,
-        document.getElementById("a2").value,
-        document.getElementById("a3").value
-    ];
+    const myFields = document.querySelectorAll("input");
+    let myObjectArray = []; // https://stackoverflow.com/questions/15742442/declaring-array-of-objects
     
-    // Loop through myAttributes. If any input field is blank, display a message under the button and break out of validateElements.
-    for (let a of myAttributes)
+    // If any input field is blank, display a message under the button and break out of validateElements.
+    // Indexed iteration in for-of loop code inspired by https://stackoverflow.com/a/10179849
+    for (let [i, f] of myFields.entries())
     {
-        if (a == "")
+        if (f.value == "")
         {
-            document.getElementById("output").innerHTML = "Please fill in all attributes.";
+            document.getElementById("output").innerHTML = "Please fill in all fields.";
             return false;
         }
+        // Custom key inspiration:  https://www.geeksforgeeks.org/how-to-use-a-variable-for-a-key-in-a-javascript-object-literal/
+        let objectKey = f.labels[0].textContent;
+        let myObject = {};
+        myObject[objectKey] = f.value;
+        myObjectArray.push(myObject);
     }
     
-    // If all input fields have data, generate and display a syntactically valid JSON array under the button.
-    generateJSON();
+    // If all input fields have data, generate and display syntactically valid JSON under the button.
+    generateJSON(myObjectArray);
     return true;
     
-    // generateJSON() is an inner function, which keeps code organized and allows use of myAttributes.
-    function generateJSON()
+    // generateJSON() is an inner function, which keeps code organized.
+    function generateJSON(myData)
     {
-        let myAttributesJSON = JSON.stringify(myAttributes);
-        document.getElementById("output").innerHTML = myAttributesJSON;
+        document.getElementById("output").innerHTML = JSON.stringify(myData);
     }
 }
