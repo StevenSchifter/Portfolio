cad5172b972cf9eb1299fb7a04a9832f24e52558
Steven Schifter <70248147+StevenSchifter@users.noreply.github.com>
2024-03-20 23:57:49 -0400

    Generating a nested JSON array is now possible
    HTML: Form label and field alignment fixed; indentation field added; additional button for generating nested JSON; default values provided for quick demos
    CSS: Reorganized; removed color names in favor of hex codes
    JS: Allowed buttons aligned in form to perform expected actions by preventing default page refresh; supports custom indentation for nested JSON

--- ArrayToJSON/ArrayToJSON.js
+++ ArrayToJSON/ArrayToJSON.js
@@ -1,9 +1,14 @@
 "use strict";
 
+const gfb = document.querySelector("#generateFlatButton");
+const gnb = document.querySelector("#generateNestedButton");
+gfb.addEventListener("click", event => {event.preventDefault(); validateFields();});  
+gnb.addEventListener("click", event => {event.preventDefault(); validateFields(true);});  
+
 // This function checks whether any of the input fields are blank.
-function validateFields()
+function validateFields(useNesting = false)
 {
-    const myFields = document.querySelectorAll("input");
+    const myFields = document.querySelectorAll(".data");
     let myObjectArray = []; // https://stackoverflow.com/questions/15742442/declaring-array-of-objects
     
     // If any input field is blank, display a message under the button and break out of validateElements.
@@ -16,19 +21,34 @@ function validateFields()
             return false;
         }
         // Custom key inspiration:  https://www.geeksforgeeks.org/how-to-use-a-variable-for-a-key-in-a-javascript-object-literal/
-        let objectKey = f.labels[0].textContent;
         let myObject = {};
+        let objectKey = f.labels[0].textContent;
         myObject[objectKey] = f.value;
         myObjectArray.push(myObject);
     }
     
     // If all input fields have data, generate and display syntactically valid JSON under the button.
-    generateJSON(myObjectArray);
+    if (useNesting)
+    {       
+        let indentation = parseInt(document.querySelector("#indentation").value);
+        generateJSON(myObjectArray, indentation);
+    }
+    else
+    {
+        generateJSON(myObjectArray, 0);
+    }
     return true;
     
     // generateJSON() is an inner function, which keeps code organized.
-    function generateJSON(myData)
+    function generateJSON(myData, indentation)
     {
-        document.getElementById("output").innerHTML = JSON.stringify(myData);
+        if (!indentation)
+        {
+            document.getElementById("output").innerHTML = `<pre>${JSON.stringify(myData)}</pre>`;
+        }
+        else
+        {
+            document.getElementById("output").innerHTML = `<pre>${JSON.stringify(myData, null, indentation)}</pre>`;
+        }
     }
 }
